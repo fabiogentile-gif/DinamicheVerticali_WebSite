4e5436839bdcb04f81f69d528fabf091b8e22ed8
Fabio Gentile <fabio.gentile@edu-its.it>
2026-06-23 17:51:27 +0200

    Finito il footer inizio creazione pagina

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,22 @@ export default function Home() {
   return (
     <div>
       <main>
-        <p>testo</p>
+        <section className="flex flex-col justify-center ">
+          <h1 className="text-amber-600 my-10 text-center">CALENDARIO PROSSIMI CORSI</h1>
+          <div>Barra di ricerca</div>
+          <div>
+            <aside>
+              <h3>Filtra i corsi</h3>
+              <div>
+                <ul>
+                  <li>
+                    ciao
+                  </li>
+                </ul>
+              </div>
+            </aside>
+          </div>
+        </section>
       </main>
     </div>
   );
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -2,39 +2,57 @@ import Image from "next/image";
 
 export default function Footer() {
     return (
-        <footer className="flex p-5 justify-between border-t-3 border-amber-500">
-            <div>
-                <Image
-                    src="/logo/logo-dinamiche-verticali-formazione.svg"
-                    width={200}
-                    height={200}
-                    alt="logo-dinamiche-verticali-formazione" />
-                <ul>
-                    <li>
-                        © 2026 Dinamiche Verticali Formazione srl
-                    </li>
-                    <li>
-                        P.iva 11991170017
-                    </li>
-                </ul>
+        <footer className=" border-t-3 border-amber-500">
+            <div className="flex p-5 justify-around items-center mx-20">
+                <div>
+                    <Image
+                        src="/logo/logo-dinamiche-verticali-formazione.svg"
+                        width={200}
+                        height={200}
+                        alt="logo-dinamiche-verticali-formazione" />
+                    <ul>
+                        <li>
+                            © 2026 Dinamiche Verticali Formazione srl
+                        </li>
+                        <li>
+                            P.iva 11991170017
+                        </li>
+                    </ul>
+                </div>
+                <div className="flex gap-6 justify-center">
+                    <div className="relative h-16 w-32">
+                        <Image
+                            src="/logo/logo-global-wind-organisation.avif"
+                            width={100}
+                            height={100}
+                            alt="logo-global-wind-organisation" />
+                    </div>
+                    <div className="relative h-16 w-32">
+                        <Image
+                            src="/logo/logo-irata-international.avif"
+                            width={100}
+                            height={100}
+                            alt="logo-global-wind-organisation" />
+                    </div>
+                    <div className="relative h-16 w-32">
+                        <Image
+                            src="/logo/logo-petzl-technical-institute.avif"
+                            width={100}
+                            height={100}
+                            alt="logo-petzl-technical-institute" />
+                    </div>
+                </div>
+                <div>
+                    <h3 className="text-xl font-semibold">Contattaci</h3>
+                    <div>
+                        <h3 className="text-amber-500 text-xl">+39 011 27 32 500</h3>
+                        <p className="text-sm">(dalle 08:30 alle 17:30 ; Lun-Ven)</p>
+                        <p>formazione@petzl.it</p>
+                        <p>Via G. Battista Feroggio, 54, 10151 Torino</p>
+                    </div>
+                </div>
             </div>
-            <div className="flex auto">
-                <Image
-                    src="/logo/logo-global-wind-organisation.avif"
-                    width={100}
-                    height={50}
-                    alt="logo-global-wind-organisation" />
-                <Image
-                    src="/logo/logo-irata-international.avif"
-                    width={100}
-                    height={50}
-                    alt="logo-global-wind-organisation" />
-                <Image
-                    src="/logo/logo-petzl-technical-institute.avif"
-                    width={100}
-                    height={50}
-                    alt="logo-petzl-technical-institute" />
-            </div>
+
 
         </footer>
     )
